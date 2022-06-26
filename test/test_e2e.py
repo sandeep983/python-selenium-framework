0000613884920b73b838cc4fb08a55824379e089
sandeep983 <sandeeppatel983@hotmail.com>
2022-06-26 22:04:12 +0530

    update

--- test/test_e2e.py
+++ test/test_e2e.py
@@ -2,29 +2,46 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from utilities.BaseClass import BaseClass
+from pageObjects.HomePage import HomePage
+from pageObjects.CheckoutPage import CheckOutPage
+from pageObjects.ConfirmPage import ConfirmPage
 
 class TestOne(BaseClass):
     def test_e2e(self):
-        # we defined browser opening inside the 'setup method' in 'conftest.py'
-        #  file we will use it with the help of fixture.
+        '''We defined browser opening inside the 'setup method' in 'conftest.py'
+            file we will use it with the help of fixture.'''
         
         wait = WebDriverWait(self.browser, 10)
 
+        #passing browser/driver to homepage.py
+        homePage = HomePage(self.browser)
         #click shop button
-        self.browser.find_element(By.CSS_SELECTOR, "a[href*=shop]").click()
+        homePage.getShopItems().click()
+        
 
-        #getting all the products in page
-        products = self.browser.find_elements(By.XPATH, "//div[@class='card h-100']")
+        #passing the browser/driver to checkoutpage.py
+        checkOutPage = CheckOutPage(self.browser)
+        #getting all the products/cards in page
+        products = checkOutPage.getCardTitles()
+        i = -1
         for product in products:
-            if product.find_element(By.XPATH, "div/h4/a").text == 'Blackberry':
-                product.find_element(By.XPATH, "div/button").click()
+            i = i + 1
+            productTitle = product.text
+            if (productTitle ==  'Blackberry'):
+                checkOutPage.getAddToCart()[i].click()
+                
+        #click checkout button
+        checkOutPage.getCheckoutButton().click()
+        #checkout button of next page
+        checkOutPage.getCheckoutButtonNext().click()
 
-        #checkout button
-        self.browser.find_element(By.CSS_SELECTOR, "a[class*='btn-primary']").click()
-        self.browser.find_element(By.CSS_SELECTOR, "button[class*='btn-success']").click()
+        
 
+        #passing the browser/driver to confirmpage.py
+        confirmPage = ConfirmPage(self.browser)
         #input box - entering "ind"
-        self.browser.find_element(By.CSS_SELECTOR, "#country").send_keys('ind')
+        confirmPage.getInputBox().send_keys('ind')
+
 
         #wait till the link text India is present and then click on it
         wait.until(EC.presence_of_element_located((By.LINK_TEXT, "India")))
